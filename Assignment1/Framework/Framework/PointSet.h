33c4c40a844945a53847e3d52eeaaa1bc02bf254
Ahmed Adnan <ahmed.zangana@yahoo.com>
2025-05-20 23:27:35 +0200

    3.1 done

--- Assignment1/Framework/Framework/PointSet.h
+++ Assignment1/Framework/Framework/PointSet.h
@@ -1,10 +1,35 @@
 #ifndef POINTSET_H
 #define POINTSET_H
 
-class PointSet
-{
+#include <QVector4D>
+#include <QVector3D>
+#include <vector>
+#include <utility>
+
+class PointSet {
 public:
-    PointSet();
+    // Constructors
+    PointSet(); // empty
+    PointSet(const std::vector<QVector4D>& points);
+
+    // Accessors
+    const std::vector<QVector4D>& getPoints() const;
+    const QVector3D& getMinCorner() const;
+    const QVector3D& getMaxCorner() const;
+
+    // Utility
+    size_t size() const;
+    bool empty() const;
+
+    PointSet extractSubsetInside(const QVector3D& minCorner, const QVector3D& maxCorner) const;
+
+
+private:
+    std::vector<QVector4D> points;
+    QVector3D minCorner;
+    QVector3D maxCorner;
+
+    void computeBoundingBox(); // called internally
 };
 
 #endif // POINTSET_H
